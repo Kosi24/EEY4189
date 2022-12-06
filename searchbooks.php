4073bffadde1b75eeb5370d0b44d1dcc696b910f
M.Kosika <117499499+Kosi24@users.noreply.github.com>
2022-12-07 03:53:01 +0530

    Create searchbooks.php

---
+++ searchbooks.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <body bgcolor="black">
+    <center><h2>Library Management System</h2></center>
+    <form action = "booklist.php" method="get">
+        <center>Enter the Book Title :
+        <input type="text" name="search" size="48">
+        <br></br>
+        <input type="submit" value="submit">
+        <input type="reset" value="Reset">
+        </center>
+        <br>
+        </form>
+    </body>
+</html>
